102aebe9344912a22bee54e121d984c4db077603
Matthias <5011972+fasmat@users.noreply.github.com>
2023-02-24 11:34:01 +0000

    Assert instead of log

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"syscall/js"
 	"testing"
 
@@ -10,22 +9,26 @@ import (
 )
 
 func Test_Generate(t *testing.T) {
-	seed := js.Global().Get("Uint8Array").New(ed25519.SeedSize)
-	n := js.CopyBytesToJS(seed, make([]byte, ed25519.SeedSize))
+	seed := make([]byte, ed25519.SeedSize)
+
+	seedBytes := js.Global().Get("Uint8Array").New(ed25519.SeedSize)
+	n := js.CopyBytesToJS(seedBytes, seed)
 	require.Equal(t, ed25519.SeedSize, n)
 
-	GenerateKeyCallback.Invoke(seed, js.FuncOf(func(this js.Value, args []js.Value) any {
+	key := ed25519.NewKeyFromSeed(seed)
+
+	GenerateKeyCallback.Invoke(seedBytes, js.FuncOf(func(this js.Value, args []js.Value) any {
 		t.Log("GenerateKeyCallback invoked")
 
 		pubKey := make([]byte, ed25519.PublicKeySize)
 		n := js.CopyBytesToGo(pubKey, args[0])
 		require.Equal(t, ed25519.PublicKeySize, n)
-		t.Log("args[0]:", hex.EncodeToString(pubKey))
+		require.Equal(t, key.Public(), pubKey)
 
 		privKey := make([]byte, ed25519.PrivateKeySize)
 		n = js.CopyBytesToGo(privKey, args[1])
 		require.Equal(t, ed25519.PrivateKeySize, n)
-		t.Log("args[1]:", hex.EncodeToString(privKey))
+		require.Equal(t, key, privKey)
 
 		return nil
 	}))
