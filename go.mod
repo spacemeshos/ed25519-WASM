f70482e7f1df55b8fea843db4c6081fad6994563
Matthias <5011972+fasmat@users.noreply.github.com>
2023-02-24 10:31:21 +0000

    Update to use new ed25519 library

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/spacemeshos/ed25519-WASM
 
 go 1.19
 
-require github.com/spacemeshos/ed25519 v0.1.1
+require github.com/spacemeshos/ed25519 v0.1.2-0.20230224092249-82d338fbae79
--- main.go
+++ main.go
@@ -13,60 +13,128 @@ func init() {
 	c = make(chan bool)
 }
 
-func TypedArrayToByteSlice(arg js.Value) []byte {
-	length := arg.Length()
-	bytesToReturn := make([]byte, length)
-	for i := 0; i < length; i++ {
-		bytesToReturn[i] = byte(arg.Index(i).Int())
-	}
-	return bytesToReturn
-}
-
-var GenerateKeyCallback = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-	seed := TypedArrayToByteSlice(args[0])
-	seedBuffer := bytes.NewReader(seed)
-	publicKey, privateKey, err := ed25519.GenerateKey(seedBuffer)
+var GenerateKeyCallback = js.FuncOf(func(this js.Value, args []js.Value) any {
 	callback := args[1]
+
+	seed := make([]byte, ed25519.SeedSize)
+	n := js.CopyBytesToGo(seed, args[0])
+	if n != ed25519.SeedSize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+
+	publicKey, privateKey, err := ed25519.GenerateKey(bytes.NewReader(seed))
 	if err != nil {
 		callback.Invoke(js.Null(), js.Null())
 		return nil
 	}
-	callback.Invoke(js.TypedArrayOf([]byte(publicKey)), js.TypedArrayOf([]byte(privateKey)))
+	pubBytes := js.Global().Get("Uint8Array").New(ed25519.PublicKeySize)
+	n = js.CopyBytesToJS(pubBytes, publicKey)
+	if n != ed25519.PublicKeySize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+	privBytes := js.Global().Get("Uint8Array").New(ed25519.PrivateKeySize)
+	n = js.CopyBytesToJS(privBytes, privateKey)
+	if n != ed25519.PrivateKeySize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+
+	callback.Invoke(pubBytes, privBytes)
 	return nil
 })
 
-var DerivePrivateKeyCallback = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-	seed := TypedArrayToByteSlice(args[0])
-	index := uint64(args[1].Int())
-	salt := TypedArrayToByteSlice(args[2])
+var DerivePrivateKeyCallback = js.FuncOf(func(this js.Value, args []js.Value) any {
 	callback := args[3]
+
+	seed := make([]byte, ed25519.SeedSize)
+	n := js.CopyBytesToGo(seed, args[0])
+	if n != ed25519.SeedSize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+
+	index := uint64(args[1].Int())
+	salt := make([]byte, args[2].Length())
+	n = js.CopyBytesToGo(seed, args[2])
+	if n != args[2].Length() {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+
 	privateKey := ed25519.NewDerivedKeyFromSeed(seed, index, salt)
-	publicKey := make([]byte, ed25519.PublicKeySize)
-	copy(publicKey, privateKey[32:])
-	callback.Invoke(js.TypedArrayOf([]byte(publicKey)), js.TypedArrayOf([]byte(privateKey)))
+	pubBytes := js.Global().Get("Uint8Array").New(ed25519.PublicKeySize)
+	n = js.CopyBytesToJS(pubBytes, privateKey[32:])
+	if n != ed25519.PublicKeySize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+	privBytes := js.Global().Get("Uint8Array").New(ed25519.PrivateKeySize)
+	n = js.CopyBytesToJS(privBytes, privateKey)
+	if n != ed25519.PrivateKeySize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+	callback.Invoke(pubBytes, privBytes)
 	return nil
 })
 
-var Sign2Callback = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-	sk := ed25519.PrivateKey(TypedArrayToByteSlice(args[0]))
-	message := TypedArrayToByteSlice(args[1])
-	callback := args[len(args)-1:][0]
+var Sign2Callback = js.FuncOf(func(this js.Value, args []js.Value) any {
+	callback := args[len(args)-1]
+	
+	sk := make([]byte, ed25519.PrivateKeySize)
+	n := js.CopyBytesToGo(sk, args[0])
+	if n != ed25519.PrivateKeySize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+	message := make([]byte, args[1].Length())
+	n = js.CopyBytesToGo(message, args[1])
+	if n != args[1].Length() {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+	
 	signature := ed25519.Sign2(sk, message)
-	callback.Invoke(js.TypedArrayOf(signature))
+	sigBytes := js.Global().Get("Uint8Array").New(ed25519.SignatureSize)
+	n = js.CopyBytesToJS(sigBytes, signature)
+	if n != ed25519.SignatureSize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+	callback.Invoke(sigBytes)
 	return nil
 })
 
-var Verify2Callback = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-	pk := ed25519.PublicKey(TypedArrayToByteSlice(args[0]))
-	message := TypedArrayToByteSlice(args[1])
-	signature := TypedArrayToByteSlice(args[2])
+var Verify2Callback = js.FuncOf(func(this js.Value, args []js.Value) any {
+	callback := args[len(args)-1]
+
+	pk := make([]byte, ed25519.PublicKeySize)
+	n := js.CopyBytesToGo(pk, args[0])
+	if n != ed25519.PublicKeySize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+	message := make([]byte, args[1].Length())
+	n = js.CopyBytesToGo(message, args[1])
+	if n != args[1].Length() {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+	signature := make([]byte, ed25519.SignatureSize)
+	n = js.CopyBytesToGo(signature, args[2])
+	if n != ed25519.SignatureSize {
+		callback.Invoke(js.Null(), js.Null())
+		return nil
+	}
+
 	isValid := ed25519.Verify2(pk, message, signature)
-	callback := args[len(args)-1:][0]
 	callback.Invoke(isValid)
 	return nil
 })
 
-var ShutdownCallback = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+var ShutdownCallback = js.FuncOf(func(this js.Value, args []js.Value) any {
 	c <- true
 	return nil
 })
